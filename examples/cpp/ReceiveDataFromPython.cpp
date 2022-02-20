9b4a9c10dac7c04a3679edfe7653b3ca4d01c99b
billy <billy.saputra@motional.com>
2022-02-20 20:41:17 +0800

    finished python to cpp

--- examples/cpp/ReceiveDataFromPython.cpp
+++ examples/cpp/ReceiveDataFromPython.cpp
@@ -1,8 +1,11 @@
 #include "cnpy/cnpy.h"
+#include <Eigen/Dense>
 #include <boost/program_options.hpp>
 #include <iostream>
 #include <npy.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
 
 namespace po = boost::program_options;
 
@@ -18,15 +21,27 @@ int main(int argc, char** argv)
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
     std::cout << "opening: " << filepath << std::endl;
-
     cnpy::npz_t npz = cnpy::npz_load(filepath);
+
+    // --- Processing Normal (Numpy) Matrix
+    auto numpyArray = npz["ht_matrix"];
+    Eigen::MatrixXd eigenMat =
+        Eigen::Map<Eigen::MatrixXd>(numpyArray.data<double>(), numpyArray.shape[1], numpyArray.shape[0]);
+    // need the following if the matrix is stored with C-order
+    // eigenMat.transposeInPlace();
+    std::cout << "Numpy matrix (as Eigen matrix): " << std::endl << eigenMat << std::endl;
+
+    // ---
+
+    // Processing Image (OpenCV) Matrix
+
     cnpy::NpyArray npzImageArray = npz["img"];
     // need to know data type before hand
     // can be fixed with modifying the cnpy library
     // the typename is actually stored in the file.
-    auto* data = npzImageArray.data<uint8_t>();
+    auto* imageData = npzImageArray.data<uint8_t>();
 
-    std::cout << "mat shape: ";
+    std::cout << "image mat shape: ";
     for (const auto& s : npzImageArray.shape) {
         std::cout << s << ",";
     }
@@ -36,17 +51,47 @@ int main(int argc, char** argv)
     // see https://www.geeksforgeeks.org/display-numpy-array-in-fortran-order
     // for the ordering of data
     // by default numpy save in C-order (row-major)
+    // Note: there may be a way to avoid copy using constructor:
+    //       cv::Mat(const std::vector< _Tp > &vec, bool copyData=false)
     for (auto i = 0; i < npzImageArray.shape[0]; i++) {
         for (auto j = 0; j < npzImageArray.shape[1]; j++) {
             auto& p = image.at<cv::Vec3b>(i, j);
 
-            p[0] = data[i * npzImageArray.shape[1] * 3 + j * 3];
-            p[1] = data[i * npzImageArray.shape[1] * 3 + j * 3 + 1];
-            p[2] = data[i * npzImageArray.shape[1] * 3 + j * 3 + 2];
+            p[0] = imageData[i * npzImageArray.shape[1] * 3 + j * 3];
+            p[1] = imageData[i * npzImageArray.shape[1] * 3 + j * 3 + 1];
+            p[2] = imageData[i * npzImageArray.shape[1] * 3 + j * 3 + 2];
         }
     }
 
     cv::imshow("Image", image);
     cv::waitKey(0);
+
+    // ---
+
+    // --- Processing PointCloud (PCL) Matrix
+    cnpy::NpyArray pointCloudArray = npz["pcd_numpy"];
+    auto* pointCloudData = pointCloudArray.data<double>();
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    // pack r/g/b into rgb
+    const std::uint8_t r = 0, g = 255, b = 0;
+    std::uint32_t rgb = ((std::uint32_t)r << 16 | (std::uint32_t)g << 8 | (std::uint32_t)b);
+    for (auto i = 0; i < pointCloudArray.shape[0]; i++) {
+        // for (auto j = 0; j < pointCloudArray.shape[1]; j++) {
+            pcl::PointXYZRGB point;
+            point.x = pointCloudData[i*pointCloudArray.shape[1]];
+            point.y = pointCloudData[i*pointCloudArray.shape[1]+1];
+            point.z = pointCloudData[i*pointCloudArray.shape[1]+2];
+            point.rgb  = *reinterpret_cast<float*>(&rgb);
+        // }
+        cloud->push_back(point);
+    }
+
+    pcl::visualization::CloudViewer viewer("Cloud Viewer");
+    viewer.showCloud(cloud);
+    while (!viewer.wasStopped()) {
+    }
+
+    // ---
+
     return 0;
 }
--- examples/python/send_data.py
+++ examples/python/send_data.py
@@ -12,9 +12,17 @@ import argparse
 from transforms3d.affines import compose
 from transforms3d.euler import euler2mat
 
+# --- Normal Numpy Matrix
+
 ht_matrix = compose([20, 30, 40], euler2mat(
     np.pi/2, np.pi/2, np.pi/2), np.ones(3), np.zeros(3))
+# store as F-order to avoid transposing on the receiver side
+ht_matrix = np.asfortranarray(ht_matrix)
 print(f'homogeneous transformation matrix:\n{ht_matrix}')
+
+# ---
+
+# --- Image (OpenCV) Matrix
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 image_filepath = script_path + '/../data/car.jpeg'
@@ -24,20 +32,28 @@ print(f'opening image: {image_filepath}')
 img = cv2.imread(image_filepath)
 cv2.imshow('car', img)
 cv2.waitKey()
+print(
+    f'the image in python is of type: {type(img)} that contains {img.dtype.name}')
+
+# ---
+
+# --- PointCloud (PCL) Matrix
 
 pcd_filepath = script_path + '/../data/bunny.pcd'
 pcd = o3d.io.read_point_cloud(pcd_filepath)
 pcd_numpy = np.asarray(pcd.points)
-print(f'points in pcd as array:\n{np.asarray(pcd.points)}')
+print(
+    f'points in pcd as array:\n{np.asarray(pcd.points)}\nthese of type: {pcd_numpy.dtype.name}')
 o3d.visualization.draw_geometries([pcd])
+
+
+# ---
 
 output = io.BytesIO()
 np.savez(output, img=img, pcd_numpy=pcd_numpy)
 npzoutput = io.FileIO('data.npz', 'w')
 np.savez(npzoutput, img=img, pcd_numpy=pcd_numpy, ht_matrix=ht_matrix)
 
-print(
-    f'the image in python is of type: {type(img)} that contains {img.dtype.name}')
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", "--output", action="store_true",
